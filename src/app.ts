d7cde2e3b3b42136938f215f6fd1ff5c60288853
nguyenvanminhhoang <nguyenvanminhhoang.dl49@gmail.com>
2022-06-15 13:50:34 +0700

    Handle middleware.

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,25 @@
 import { Route } from "core/interfaces";
 import express from "express";
 import mongoose from "mongoose";
+import hpp from "hpp";
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
+import { Logger } from "./core/utils";
 
 class App {
   public app: express.Application;
   public port: string | number;
+  public production: boolean;
 
   constructor(routes: Route[]) {
     this.app = express();
     this.port = process.env.PORT || 5000;
+    this.production = process.env.NODE_ENV == "production" ? true : false;
 
     this.initializeRoutes(routes);
     this.connectToMongo();
+    this.initializeMiddleware();
   }
 
   private initializeRoutes(routes: Route[]) {
@@ -20,23 +28,33 @@ class App {
     });
   }
 
-  private connectToMongo() {
-    try {
-      const connectionString = process.env.MONGODB_URI;
-      if(!connectionString) {
-        console.log("Connection string is invalid");
-        return;
-      }
-      mongoose.connect(connectionString);
-      console.log("Mongoose is connected...");
-    } catch (error) {
-      console.log("Can't connect to MongoDB, please check and try again...");
+  private initializeMiddleware() {
+    if (this.production == true) {
+      this.app.use(hpp());
+      this.app.use(helmet());
+      this.app.use(morgan("combined"));
+      this.app.use(cors({ origin: "your.domain.com", credentials: true }));
+    } else {
+      this.app.use(morgan("dev"));
+      this.app.use(cors({ origin: "your.domain.com", credentials: true }));
     }
+  }
+
+  private connectToMongo() {
+    const connectionString = process.env.MONGODB_URI;
+    if (!connectionString) {
+      Logger.error("Connection string is invalid");
+      return;
+    }
+    mongoose.connect(connectionString).catch((reason) => {
+      Logger.error(reason);
+    });
+    Logger.info("Mongoose is connected...");
   }
 
   public listen() {
     this.app.listen(this.port, () => {
-      console.log(`Server is running at port ${this.port}`);
+      Logger.info(`Server is running at port ${this.port}`);
     });
   }
 }
